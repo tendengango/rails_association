2a298aa8df76d7eeca3c571813d50a3267090bd6
tendengango <framboutel@gmail.com>
2023-03-14 10:08:04 +0000

    rails_association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
     belongs_to :addresses
     belongs_to :customers
-    has_many :Order_foods
-    has_many :foods, throught :: Order_foods
+    has_many :order_foods
+    has_many :foods, through::order_foods
 end
